feccfc793979bcfcc449509ba1dfa9fdee130ac5
AKraja777 <ashokkumar7hitter@gmail.com>
2024-02-05 15:34:42 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -1,7 +1,7 @@
 <?php
 
 
-define('DOMAIN_URL', 'https://admin.colorjobs.site/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
+define('DOMAIN_URL', 'http://weagri.graymatterworks.com/'); /* chnage to your domain here - don't forget to add forward slash at the end of the URL like this "/" */
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 define('MAIN_REFER', 'ATN');
