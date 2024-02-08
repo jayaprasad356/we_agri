8fb1c02c8c2fc37f09a2277ce4169eee6e9fdd75
AKraja777 <ashokkumar7hitter@gmail.com>
2024-02-08 11:23:09 +0530

    plan_list.php

--- api/plan_list.php
+++ api/plan_list.php
@@ -36,12 +36,13 @@ $sql = "SELECT * FROM plan";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
+
 if ($num >= 1){
     foreach ($res as $row) {
         $temp['id'] = $row['id'];
         $temp['crop'] = $row['crop'];
         $temp['price'] = $row['price'];
-        $temp['image'] = DOMAIN_URL . $row['image'];
+       
         $temp['daily_income'] = $row['daily_income'];
         $temp['total_income'] = $row['total_income'];
         $temp['invite_bonus'] = $row['invite_bonus'];
