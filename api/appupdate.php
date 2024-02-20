419b8d9222c638a7139b2f88077840184ede0114
AKraja777 <ashokkumar7hitter@gmail.com>
2024-02-20 14:43:19 +0530

    appupdate.php

--- api/appupdate.php
+++ api/appupdate.php
@@ -11,26 +11,21 @@ include_once('../includes/crud.php');
 
 $db = new Database();
 $db->connect();
-include_once('../includes/functions.php');
-$fn = new functions;
 
-$app_version = (isset($_POST['app_version']) && $_POST['app_version'] != "") ? $db->escapeString($_POST['app_version']) : 0;
 $sql = "SELECT * FROM app_settings";
 $db->sql($sql);
-$set = $db->getResult();
-$version = $set[0]['version'];
-$res = array();
-if($app_version < $version){
-    $response['success'] = false;
-    $response['message'] = "Please update your app to the latest version.";
-    $response['data'] = $set;
-    print_r(json_encode($response));
+$res= $db->getResult();
+$num = $db->numRows($res);
 
+if ($num >= 1){
+    $response['success'] = true;
+    $response['message'] = "App Updates Retrieved Successfully";
+    $response['data'] = $res;
+    print_r(json_encode($response));
 }
 else{
-    $response['success'] = true;
-    $response['message'] = "Your app is up to date.";
+    $response['success'] = false;
+    $response['message'] = "App Updates Not Found";
     print_r(json_encode($response));
-}
 
-?>
+}
