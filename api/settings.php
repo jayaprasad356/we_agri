c416526d287a9f4daca265d46718f26a572c2087
jayaprasad356 <jayaprasad356@gmail.com>
2024-02-25 13:45:52 +0530

    set

--- api/settings.php
+++ api/settings.php
@@ -12,7 +12,7 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
      
-$sql = "SELECT * FROM settings WHERE id=1";
+$sql = "SELECT * FROM settings";
 $db->sql($sql);
 $res= $db->getResult();
 $num = $db->numRows($res);
@@ -22,23 +22,11 @@ if ($num >= 1){
     $temp = array();
     foreach ($res as $row) {
         $temp['id'] = $row['id'];
-        $temp['withdrawal_status'] = $row['withdrawal_status'];
-        $temp['contact_us'] = $row['contact_us'];
+        $temp['whatsapp_group'] = $row['whatsapp_group'];
+        $temp['telegram_channel'] = $row['telegram_channel'];
+        $temp['max_withdrawal'] = $row['max_withdrawal'];
         $temp['min_withdrawal'] = $row['min_withdrawal'];
-        $temp['image'] = DOMAIN_URL . $row['image'];
-        $temp['watch_ad_status'] = $row['watch_ad_status'];
-        $temp['offer_image'] = $row['offer_image'];
-        $temp['refer_bonus'] = $row['refer_bonus'];
-        $temp['whatspp_group_link'] = $row['whatspp_group_link'];
-        $temp['a1_job_video'] = $row['a1_job_video'];
-        $temp['post_video_url'] = DOMAIN_URL .$row['post_video_url'];
-        $temp['post_video_details'] = DOMAIN_URL . $row['post_video_details'];
-        $temp['a1_job_details'] = $row['a1_job_details'];
-        $temp['a2_job_details'] = $row['a2_job_details'];
-        $temp['a2_job_video'] = $row['a2_job_video'];
-        $temp['a1_purchase_link'] = $row['a1_purchase_link'];
-        $temp['a2_purchase_link'] = $row['a2_purchase_link'];
-        $temp['reward_ads_details'] = $row['reward_ads_details'];
+
         $rows[] = $temp;
     }
     $response['success'] = true;
