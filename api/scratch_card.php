dea0edf269d90968586096cd85d780b6b5699a47
jayaprasad356 <jayaprasad356@gmail.com>
2024-02-25 11:49:17 +0530

    scratch

---
+++ api/scratch_card.php
@@ -0,0 +1,35 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+include_once('../includes/custom-functions.php');
+include_once('../includes/functions.php');
+$fn = new functions;
+
+
+$date = date('Y-m-d');
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+
+$user_id = $db->escapeString($_POST['user_id']);
+
+$response['success'] = true;
+$response['amount'] = 250;
+$response['message'] = "Scratch Card Revealed";
+print_r(json_encode($response));
+?>
